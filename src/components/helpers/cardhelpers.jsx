bc74de12abe6768ab410fa92741df8dca8dcdcb4
AlexeyKalamkarov <kalamkarovalexey@gmail.com>
2024-10-18 15:21:36 +0300

    with TODO

--- src/components/helpers/cardhelpers.jsx
+++ src/components/helpers/cardhelpers.jsx
@@ -1,7 +1,7 @@
-import Card1 from '../../images/projects/cards/Cinema.jpg';
-import Card2 from '../../images/projects/cards/WineApp.jpg';
-import Card3 from '../../images/projects/cards/TravelApp.png';
-import Card4 from '../../images/projects/cards/Card1.png';
+import Cinema from '../../images/projects/cards/Cinema.jpg';
+import WineApp from '../../images/projects/cards/WineApp.jpg';
+import TravelApp from '../../images/projects/cards/TravelApp.png';
+import TODO from '../../images/projects/cards/TODO.png';
 import Behance from '../../images/cards/behance.svg'
 import Git from '../../images/cards/git.svg';
 
@@ -12,7 +12,7 @@ const CardMean = [
         id: 'Card1',
         title: 'Сайт онлайн кинотеатра',
         subtitle: 'Агрегатор контента, предоставляющие возможность просмотра видео как через сайты проектов, так и через приложения сервисов для мобильных платформ и Smart TV.',
-        img: Card1,
+        img: Cinema,
         icon: Behance,
         path: 'https://www.behance.net/gallery/155522085/Streaming-service'
     },
@@ -20,7 +20,7 @@ const CardMean = [
         id: 'Card2',
         title: 'Мобильное приложение WineApp',
         subtitle: 'Мобильное приложение по подбору вин по критериям и рейтингу. В приложении вы можете по фотографии определить рейтинг напитка. В приложении можно вести дневник с заметками по всем винам. ',
-        img: Card2,
+        img: WineApp,
         icon: Behance,
         path: 'https://www.behance.net/gallery/157710669/Wine-AppMobile-App'
     },
@@ -28,23 +28,23 @@ const CardMean = [
         id: 'Card3',
         title: 'Социальная сеть & месседжер',
         subtitle: 'Мобильное приложение для туристов. Позволяет определиться с местом и временем путешествия. Через него вы можетн забронировать отель, купить билеты на самолет или поезд, записаться на экскурсии.',
-        img: Card3,
+        img: TravelApp,
         icon: Behance,
         path: 'https://www.behance.net/gallery/161044079/Travel-appGoTravel'
     },
     {
         id: 'Card4',
-        title: 'Приложение на планшет',
-        subtitle: 'Мобильное приложение',
-        img: Card4,
+        title: 'Приложение для создания заметок',
+        subtitle: 'TODO list позволяет добавлять еовые заметки с точной датой создания, редактировать их и удалять',
+        img: TODO,
         icon: Git,
-        path: 'https://github.com/AlexeyKalamkarov'
+        path: 'https://github.com/AlexeyKalamkarov/TODO'
     },
     {
         id: 'Card5',
         title: 'Магазин',
         subtitle: 'Мобильное приложение',
-        img: Card1,
+        img: Cinema,
         icon: Behance,
         path: ''
     },
@@ -52,7 +52,7 @@ const CardMean = [
         id: 'Card6',
         title: 'Приложение',
         subtitle: 'Сайт- приложение',
-        img: Card2,
+        img: WineApp,
         icon: Behance,
         path: ''
     },
@@ -60,7 +60,7 @@ const CardMean = [
         id: 'Card7',
         title: 'Социальная сеть & месседжер',
         subtitle: 'Мобильное приложение',
-        img: Card3,
+        img: TravelApp,
         icon: Behance,
         path: ''
     },
@@ -68,7 +68,7 @@ const CardMean = [
         id: 'Card8',
         title: 'Приложение на планшет',
         subtitle: 'Мобильное приложение',
-        img: Card4,
+        img: TODO,
         icon: Behance,
         path: ''
     },
